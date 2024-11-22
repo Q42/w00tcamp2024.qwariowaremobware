8172686cca45b4939a0fccba2d6e668e808bfd89
Nino van Hooff <ninovanhooff@gmail.com>
2024-11-22 11:52:14 +0100

    ADD basic binary tree positioning

--- source/Minigames/invert_binary_tree/invert_binary_tree.lua
+++ source/Minigames/invert_binary_tree/invert_binary_tree.lua
@@ -33,22 +33,58 @@ local invert_binary_tree = {}
 -- all of the code here will be run when the minigame is loaded, so here we'll initialize our graphics and variables:
 local gfx <const> = playdate.graphics
 
--- animation for on-screen Playdate sprite
-local playdate_image_table = gfx.imagetable.new("Minigames/invert_binary_tree/images/playdate")
-local low_battery_image_table = gfx.imagetable.new("Minigames/invert_binary_tree/images/playdate_low_battery")
-local pd_sprite = gfx.sprite.new(image_table)
-
 -- update sprite's frame so that the sprite will reflect the crank's actual position
 local crank_position = playdate.getCrankPosition() -- Returns the absolute position of the crank (in degrees). Zero is pointing straight up parallel to the device
 local frame_num = math.floor( crank_position / 45 + 1 )
-pd_sprite:setImage(playdate_image_table:getImage(frame_num))
 
+-- title image
+-- Load the title screen
+local title_sprite = gfx.sprite.new(gfx.image.new("Minigames/invert_binary_tree/images/invert_binary_tree_title"))
 
-pd_sprite:moveTo(200, 120)
-pd_sprite:add()
-pd_sprite.frame = 1 
-pd_sprite.crank_counter = 0
-pd_sprite.total_frames = 16
+title_sprite:moveTo(200, 120)
+title_sprite:add()
+
+local function createNumberObject(number)
+	local numberObject = {
+		val = number,
+		spr = gfx.sprite.new(gfx.imageWithText("" .. number, 200, 200))
+	}
+	return numberObject
+end
+
+local numberStates = {}
+numberStates[1] =  { createNumberObject(1) }
+numberStates[2] =  { createNumberObject(2), createNumberObject(3) }
+
+local testNumberObject = createNumberObject(1337)
+testNumberObject.spr:moveTo(100, 50)
+testNumberObject.spr:add()
+
+local function showBinaryTreeRow(states, center_x, level)
+	local spacing = 100  - 20 * level
+	local halfSpacing = spacing / 2
+	local y = 20 + level * 80
+	for index, value in ipairs(states) do
+		local pos_x = center_x - halfSpacing + spacing * (index - 1 ) -- + 0 if index 1, + spacing if index 2
+		value.spr:moveTo(pos_x, y)
+	end
+end
+
+local function updateBinaryTree()
+	for index, value in ipairs(numberStates) do
+		showBinaryTreeRow(value, 200, index) -- todo centerPoint
+	end
+end
+
+local function showBinaryTree()
+	updateBinaryTree()
+	for index, value in ipairs(numberStates) do
+		for index2, value2 in ipairs(value) do
+			value2.spr:add()
+		end
+	end
+end
+
 
 
 --> Initialize music / sound effects
@@ -58,11 +94,11 @@ local click_noise = playdate.sound.sampleplayer.new('Minigames/invert_binary_tre
 --local victory_theme = playdate.sound.fileplayer.new('Minigames/TV_Tuner/sounds/static_noise')
 
 -- set initial gamestate and start prompt for player to hit the B button
-local gamestate = 'hitB'
+local gamestate = 'title'
 mobware.BbuttonIndicator.start()
 
 -- start timer	 
-local MAX_GAME_TIME = 6 -- define the time at 20 fps that the game will run betfore setting the "defeat"gamestate
+local MAX_GAME_TIME = 600 -- define the time at 20 fps that the game will run betfore setting the "defeat"gamestate
 local game_timer = playdate.frameTimer.new( MAX_GAME_TIME * 20, function() gamestate = "defeat" end ) --runs for 8 seconds at 20fps, and 4 seconds at 40fps
 	--> after <MAX_GAME_TIME> seconds (at 20 fps) will set "defeat" gamestate
 	--> I'm using the frame timer because that allows me to increase the framerate gradually to increase the difficulty of the minigame
@@ -77,46 +113,20 @@ local game_timer = playdate.frameTimer.new( MAX_GAME_TIME * 20, function() games
 function invert_binary_tree.update()
 
 	-- updates all sprites
-	gfx.sprite.update() 
+	gfx.sprite.update()
 
 	-- update timer
 	playdate.frameTimer.updateTimers()
 
 	-- In the first stage of the minigame, the user needs to hit the "B" button
-	if gamestate == 'hitB' then
-		if playdate.buttonIsPressed('b') then
-			-- If player hits the "B" button during this gamestate, remove B button prompt and move to next gamestate
-			mobware.BbuttonIndicator.stop()
-			mobware.AbuttonIndicator.start()
-			gamestate = 'hitA'
-		end
-
-	-- In the second stage of the minigame, the user needs to hit the "A" button
-	elseif gamestate == 'hitA' then
+	if gamestate == 'title' then
+		playdate.wait(1000)
+		title_sprite:remove()
+		updateBinaryTree()
+		showBinaryTree()
+		gamestate = 'playing'
+	elseif gamestate == 'playing' then
 		
-		if playdate.buttonIsPressed('a') then
-			-- If player hits the "A" button during this gamestate then stop the prompt move to next gamestate
-			mobware.AbuttonIndicator.stop()
-			mobware.DpadIndicator.start()
-			gamestate = 'hitDpad'
-		end
-
-	-- In the next stage of the minigame, the user needs to hit the D-pad
-	elseif gamestate == 'hitDpad' then
-		if playdate.buttonIsPressed( playdate.kButtonUp ) or playdate.buttonIsPressed( playdate.kButtonDown ) or playdate.buttonIsPressed( playdate.kButtonLeft ) or playdate.buttonIsPressed( playdate.kButtonRight )  then
-			-- If player hits the D-pad during this gamestate then move to next gamestate
-			mobware.DpadIndicator.stop()
-			mobware.crankIndicator.start()
-			gamestate = 'turnCrank'
-		end
-
-	-- In the final stage of the minigame the user needs to turn the crank
-	elseif gamestate == 'turnCrank' then
-		if math.abs(playdate.getCrankTicks(1)) >= 1 then
-			mobware.crankIndicator.stop()
-			gamestate = 'victory'
-		end
-
 
 	elseif gamestate == 'victory' then
 		-- The "victory" gamestate will simply show the victory animation and then end the minigame
@@ -133,14 +143,14 @@ function invert_binary_tree.update()
 	elseif gamestate == 'defeat' then
 
 		-- if player has lost, show images of playdate running out of power 
-		local playdate_low_battery_image = gfx.image.new("Minigames/invert_binary_tree/images/playdate_low_battery")
-		local low_battery = gfx.sprite.new(playdate_low_battery_image)
-		low_battery:moveTo(150, 75)
-		low_battery:addSprite()
-		gfx.sprite.update() 
+		-- local playdate_low_battery_image = gfx.image.new("Minigames/invert_binary_tree/images/playdate_low_battery")
+		-- local low_battery = gfx.sprite.new(playdate_low_battery_image)
+		-- low_battery:moveTo(150, 75)
+		-- low_battery:addSprite()
+		-- gfx.sprite.update() 
 
-		-- wait another 2 seconds then exit
-		playdate.wait(2000)	-- Pause 2s before ending the minigame
+		-- -- wait another 2 seconds then exit
+		-- playdate.wait(2000)	-- Pause 2s before ending the minigame
 
 		-- return 0 to indicate that the player has lost and exit the minigame 
 		return 0
