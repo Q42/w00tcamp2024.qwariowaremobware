3ba2b66d608cbcd0fa3282ffffcdee0b7b0f23bc
Joost Naaijen <joost@naaijen.com>
2024-11-22 19:59:17 +0100

    Make mara move

--- source/Minigames/geit/geit.lua
+++ source/Minigames/geit/geit.lua
@@ -1,4 +1,4 @@
-
+-- import 'lib/AnimatedSprite' --used to generate animations from spritesheet
 --[[
 	Author: Andrew Loebach
 
@@ -15,8 +15,21 @@ local MAX_GAME_TIME = 1 -- define the time at 20 fps that the game will run betf
 local game_timer = playdate.frameTimer.new( MAX_GAME_TIME * 20, function() gamestate = "defeat" end ) 
 	--> after <MAX_GAME_TIME> seconds (at 20 fps) will move to "defeat" gamestate
 
-function geit.update()
 
+function geit.init()
+	-- setup background gif
+	background_gif = gfx.imagetable.new("Minigames/geit/images/mara")
+	background_sprite = AnimatedSprite.new(background_gif)
+	background_sprite:addState("animate", 1, 4, { tickStep = 2, loop = true, nextAnimation = "idle" }, true)
+	background_sprite:moveTo(120, 120)
+	background_sprite:setZIndex(1)
+
+	background_sprite:changeState("animate")
+	gfx.sprite.update()
+end
+
+function geit.update()
+	gfx.sprite.update()
 
 end
 
