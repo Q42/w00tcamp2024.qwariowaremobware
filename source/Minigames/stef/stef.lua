a228003c498e2b70fb768c2680cfcb64804372cb
Erik Deijl <erikd@q42.nl>
2024-11-23 14:04:51 +0100

    Sliding Stef

--- source/Minigames/stef/stef.lua
+++ source/Minigames/stef/stef.lua
@@ -6,7 +6,48 @@ local shakesNeeded = 20 -- Number of shakes needed to win
 
 playdate.startAccelerometer()
 
+local gfx <const> = playdate.graphics
+local stefImage = gfx.image.new("Minigames/stef/images/stef.png")
+assert(stefImage, "Failed to load stef image")
+
+local smots1Img = gfx.image.new("Minigames/stef/images/smots1.png")
+assert(smots1Img, "Failed to load smots1 image")
+local smotsSprite = gfx.sprite.new(smots1Img)
+
+local stefSprite = gfx.sprite.new(stefImage)
+local screenWidth, screenHeight = playdate.display.getSize()
+local imageWidth, imageHeight = stefImage:getSize()
+local startY = screenHeight + imageHeight / 2
+local endY = screenHeight - screenHeight / 4
+local returnY = screenHeight
+
+stefSprite:moveTo(screenWidth / 2, startY)
+stefSprite:add()
+
+local animationTimer = playdate.timer.new(2000, startY, endY, playdate.easingFunctions.linear)
+animationTimer.updateCallback = function(timer)
+  local y = timer.value
+  stefSprite:moveTo(screenWidth / 2, y)
+end
+animationTimer.timerEndedCallback = function(timer)
+  local returnTimer = playdate.timer.new(2000, endY, returnY, playdate.easingFunctions.linear)
+  returnTimer.updateCallback = function(timer)
+    local y = timer.value
+    stefSprite:moveTo(screenWidth / 2, y)
+  end
+  returnTimer.timerEndedCallback = function(timer)
+    -- gfx.sprite.setBackgroundDrawingCallback(
+    --   function(x, y, width, height)
+    --     stefImage:draw(0, 0)
+    --   end
+    -- )
+    -- stefSprite:remove()
+  end
+end
+
 function stef.update()
+  gfx.sprite.update()
+  playdate.timer.updateTimers()
   -- Get the accelerometer data
   local ax, ay, az = playdate.readAccelerometer()
 
