e56d3cd179751094507ddbc0c54ac71871bb27ce
Jan van Overbeek <jan@q42.nl>
2024-11-23 13:14:35 +0100

    Added collision stuff

--- source/Minigames/dino_the_game/dino_the_game.lua
+++ source/Minigames/dino_the_game/dino_the_game.lua
@@ -15,6 +15,7 @@ mobware.crankIndicator.start()
 -- Make sound
 local finish_sound = playdate.sound.sampleplayer.new('Minigames/dino_the_game/sounds/tada.wav')
 local internet_startup_sound = playdate.sound.sampleplayer.new('Minigames/dino_the_game/sounds/internet_startup.wav')
+local cry_sound = playdate.sound.sampleplayer.new('Minigames/dino_the_game/sounds/cry.wav')
 
 -- Initialize graphics
 local dino_image_table = gfx.imagetable.new("Minigames/dino_the_game/images/dino")
@@ -85,25 +86,46 @@ local dino_sprite = AnimatedSprite.new(dino_image_table)
 dino_sprite:addState("standing", 1, 2, { tickStep = 3, loop = true, nextAnimation = "idle" }, true)
 dino_sprite:addState("walking", 3, 4, { tickStep = 3, loop = true, nextAnimation = "idle" }, true)
 dino_sprite:moveTo(50, 175)
+dino_sprite:setZIndex(10)
 
-local function doesDinoCollideWithCactus()
-    local collisions = dino_sprite:overlappingSprites()
+local function pixelCollision(s1, s2)
+    local s1x, s1y = s1:getPosition()
+    local s2x, s2y = s2:getPosition()
 
-    for i = 1, #collisions do
-        local collision = collisions[i]
-        for _, cactus in ipairs(cactus_sprites) do
-            if collision.other == cactus.sprite then
-                return true
+    local s1w, s1h = s1:getSize()
+    local s2w, s2h = s2:getSize()
+
+    -- Calculate overlap region
+    local left = math.max(s1x - s1w / 2, s2x - s2w / 2)
+    local right = math.min(s1x + s1w / 2, s2x + s2w / 2)
+    local top = math.max(s1y - s1h / 2, s2y - s2h / 2)
+    local bottom = math.min(s1y + s1h / 2, s2y + s2h / 2)
+
+    for y = top, bottom do
+        for x = left, right do
+            local p1 = s1:getImage():sample(x - (s1x - s1w / 2), y - (s1y - s1h / 2))
+            local p2 = s2:getImage():sample(x - (s2x - s2w / 2), y - (s2y - s2h / 2))
+
+            -- P1 is dino and it has white around its black pixels so we need to check for black and white
+            if p1 == gfx.kColorBlack and p2 == gfx.kColorBlack or p1 == gfx.kColorWhite and p2 == gfx.kColorBlack then
+                return true -- Collision detected
             end
         end
     end
 
-    return false;
+    return false
 end
 
-local function jump()
-
-end;
+local function doesDinoCollideWithCactus()
+    for i, cactus in ipairs(cactus_sprites) do
+        if cactus.sprite:isVisible() then
+            if pixelCollision(dino_sprite, cactus.sprite) then
+                return true
+            end
+        end
+    end
+    return false
+end
 
 local dino_position = 50;
 local endgame = false;
@@ -126,12 +148,19 @@ local function jump()
     end
 end
 
+-- start timer
+local MAX_GAME_TIME = 20 -- define the time at 20 fps that the game will run betfore setting the "defeat" gamestate
+local game_state = "running"
+local game_timer = playdate.frameTimer.new(MAX_GAME_TIME * 20, function() game_state = "defeat" end)
+--> after <MAX_GAME_TIME> seconds (at 20 fps) will move to "defeat" gamestate
 function dino_the_game.update()
     gfx.sprite.update()
     playdate.frameTimer.updateTimers()
 
     if doesDinoCollideWithCactus() then
-        return 0;
+        cry_sound:play()
+        playdate.wait(1000)
+        return 0
     end
 
     local crankChange = playdate.getCrankChange()
@@ -169,7 +198,7 @@ function dino_the_game.update()
         dino_sprite:moveTo(50, dino_y)
     end
 
-    if playdate.buttonJustPressed("b") and not finished then
+    if playdate.buttonJustPressed("b") and not finished and not endgame then
         if mobware.BbuttonIndicator then
             mobware.BbuttonIndicator.stop()
         end
@@ -196,6 +225,12 @@ function dino_the_game.update()
         dino_position = dino_position + crankChange
     else
         dino_sprite:changeState("standing")
+    end
+
+    if game_state == "defeat" then
+        cry_sound:play()
+        playdate.wait(1700)
+        return 0
     end
 end
 
